69a32eedc47336ae6caaf8d1af5f3d1e49415b68
Bensuperpc <bensuperpc@gmail.com>
2019-06-26 10:01:18 +0200

    Huge update and some improvement
    Huge update and some improvement

--- git.py
+++ git.py
@@ -18,22 +18,38 @@ import os  # We need this module
 import os.path
 from pathlib import Path
 import sys
+import platform
 
 
-class gitpull:
+# Change to pull if you want merge local and remote
+# Change to fetch if you want download repo from remote to local
+# git push origin master #For local to remote without sync between
+# git checkout -f HEAD
+
+
+class git:
     major = "1"
     minor = "0"
     micro = "0"
     releaselevel = "final"
 
-    def gitpull(self, maindirectory, thelist):
-        # For Windows, change path
-        # print(self)
+    def setgitproxy(self):
+        os.system('git config --global --unset http.proxy')
+        # os.system('git config --global http.proxy http://dominio\\vsilva:Passw0rd@195.53.86.82:3128')
+        # os.system('git config --global http.proxy http://vsilva:Passw0rd@103.55.88.52:8080')
+        os.system(
+            "git config --global http.proxy http://dominio\vsilva:Passw0rd@103.55.88.52:8080")
+        os.system('git config --global http.proxy')
+        #  87.202.9.104:8080
+
+    def gitupdate(self, maindirectory, thelist):
+
         print('maindirectory :' + maindirectory)
 
         print('thelist :', thelist)
-
-        os.system('SET PATH=%PATH%;C:\Program Files (x86)\Git\cmd;')
+        if platform.system() == 'Windows':
+            print("SET PATH")
+            os.system('SET PATH=%PATH%;C:\Program Files (x86)\Git\cmd;')
 
         # folder that contains repository
         # maindirectory = os.path.dirname(os.path.realpath(__file__))
@@ -46,17 +62,21 @@ class gitpull:
 
         for i in range(len(repositorylist)):
             # Repository URL
-            # 'https://github.com/microsoft/Terminal.git'
             repository = repositorylist[i]
 
             # get repository name ex : dolphin, vlc
             repositoryname = repository
             repositoryname = repositoryname.replace('.git', '')
+
             repositoryname = repositoryname.replace(
                 repositoryname[:repositoryname.rindex('/') + 1], '')
-
-            # Current directory path of repository
-            directory = maindirectory + '\\' + repositoryname
+            # Change current directory
+            if platform.system() == 'Windows':
+                # Current directory path of repository
+                directory = maindirectory + '\\' + repositoryname
+            else:
+                # Current directory path of repository
+                directory = maindirectory + '/' + repositoryname
 
             # Check if current directory path of repository exist
             my_file = Path(directory)
@@ -74,9 +94,31 @@ class gitpull:
                 print("Cloning :" + repository)
                 os.system('git clone -v ' + repository)
                 print("Cloning OK")
+                
+				# Change default directory
+                os.chdir(directory)
 
                 print('Init :' + directory)
-                os.system('git init ' + directory)
+                os.system('git init ' + repository)
+                print('Init OK')
+                
+                print('fetch :' + repository)
+                os.system('git fetch --recurse-submodules -j2 -v ' + repository)
+                print('fetch OK')
+                
+                
+                os.system('git submodule update --init --recursive --remote')
+                os.system('git submodule update --recursive --remote')
+                print('Submodule : OK')
+
+                #print('Check git integrity : ...')
+                #os.system('git reflog expire --expire=now --all')
+                #os.system('git gc --prune=now')
+                #os.system('git fsck --full')
+                #print('Check git integrity : OK')
+                # os.system('git submodule update --recursive --remote')
+                # 'git submodule update --init --recursive')
+                # os.system('git submodule update --init --recursive ' + directory)
 
             else:
                 # exists
@@ -85,20 +127,63 @@ class gitpull:
                 # Change default directory
                 os.chdir(directory)
 
+                # Test if git are init
                 if os.system('git rev-parse --is-inside-work-tree') == 0:
-                    print('Pulling :' + repository)
-                    os.system('git pull -v ' + repository)
-                    print('Pulling OK')
+                    # git are init
+                    print('fetch :' + repository)
+                    os.system('git fetch --recurse-submodules -j2 -v ' + repository)
+                    print('fetch OK')
+                    
+                    #os.system('git reflog expire --expire=now --all')
+                    #os.system('git gc --prune=now')
+                    #os.system('git checkout -f .')
+                    #os.system('git reset --hard')
+                    #os.system('git fsck --full')
+                    #print('checkout : OK')
+
+                    print('Submodule...')
+                    os.system(
+                        'git submodule update --init --recursive --remote')
+                    os.system('git submodule update --recursive --remote')
+                    print('Submodule : OK')
+
+                    # print('Check git integrity : ...')
+                    # os.system('git reflog expire --expire=now --all')
+                    # os.system('git gc --prune=now')
+                    # os.system('git fsck --full')
+                    # print('Check git integrity : OK')
+
+                    # os.system('git branch')
+                    # os.system('git tag')
+                    # https://username:password@mygithost.com/my/repository
                 else:
-                    # os.system('git init C:\D\git\github\Terminal')
+                    # git aren't init
                     print('Cloning :' + repository)
                     os.system('git clone -v ' + repository)
                     print("Cloning OK")
-                    # os.system('git init C:\D\git\github\Terminal')
+
+                    print('Init :' + directory)
                     os.system('git init ' + directory)
 
-        # For Windows, change path
-        os.system('SET PATH=%PATH%;c:\windows\system32;')
+                    print('fetch :' + repository)
+                    os.system('git fetch --recurse-submodules -j2 -v ' + repository)
+                    print('fetch OK')
+
+                    print('Submodule : ....')
+                    os.system(
+                        'git submodule update --init --recursive --remote')
+                    os.system('git submodule update --recursive --remote')
+                    print('Submodule : OK')
+
+                    #print('Check git integrity : ...')
+                    #os.system('git reflog expire --expire=now --all')
+                    #os.system('git gc --prune=now')
+                    #os.system('git fsck --full')
+                    #print('Check git integrity : OK')
+        print('==========================================')
+        if platform.system() == 'Windows':
+            # For Windows, change path
+            os.system('SET PATH=%PATH%;c:\windows\system32;')
 
     def __init__(self):
         self.name = "gitclonepull"
@@ -108,18 +193,38 @@ class gitpull:
 
 
 if __name__ == '__main__':
-    objName = gitpull()
+    objName = git()
     print("Lib Version : " + objName.class_version())
     repositorylist = ['https://github.com/dolphin-emu/dolphin.git',
+                      # 'git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/disco',
                       'https://github.com/Bensuperpc/Journal_lumineux.git',
                       'https://github.com/videolan/vlc.git',
                       'https://github.com/kornelski/7z.git',
-                      'https://github.com/HandBrake/HandBrake.git']
+                      'https://github.com/KDE/kdenlive.git',
+                      'https://github.com/FFmpeg/FFmpeg.git',
+                      'https://github.com/Microsoft/vscode.git',
+                      'https://github.com/Microsoft/vscode-cpptools.git',
+                      'https://github.com/HandBrake/HandBrake.git',
+                      'https://github.com/snes9xgit/snes9x.git',
+                      'https://github.com/torvalds/linux.git',
+                      'https://github.com/Bensuperpc/DelayTime.git',
+                      'https://github.com/darktable-org/darktable.git',
+                      'https://github.com/kovidgoyal/calibre.git',
+                      'https://github.com/KiCad/kicad-source-mirror.git',
+                      'https://github.com/KiCad/kicad-symbols.git',
+                      'https://github.com/KiCad/kicad-packages3D.git',
+                      'https://github.com/FreeCAD/FreeCAD.git',
+                      'https://github.com/FreeCAD/FreeCAD-addons.git',
+					  'https://github.com/tensorflow/tensorflow.git',
+					  'https://github.com/pmmp/PocketMine-MP.git',
+					  'https://github.com/ShareX/ShareX.git',
+                      'https://github.com/EpicGames/UnrealEngine.git', ]
 
-    # objName.gitpull(repositorylist)
+    # Get current path of this script
     maindirectory = os.path.dirname(os.path.realpath(__file__))
-    objName.gitpull(maindirectory, repositorylist)
 
+    # objName.setgitproxy()
+    objName.gitupdate(maindirectory, repositorylist)
     # count the arguments
     arguments = len(sys.argv) - 1
     position = 1
@@ -144,11 +249,10 @@ if __name__ == '__main__':
 # https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions?page=1&tab=oldest#tab-top
 # https://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
 # https://stackoverflow.com/questions/32554527/typeerror-list-indices-must-be-integers-or-slices-not-str
+# https://git-scm.com/book/en/v2/Git-Basics-Tagging
+
 
 # SET PATH=%PATH%;C:\Program Files (x86)\Git\cmd;
 # c:\windows\system32;
-# git config --global --unset http.proxy
-# git config --global http.proxy http://dominio\vsilva:Passw0rd@195.53.86.82:3128
-# git config --global http.proxy
 # ping wpad
 # directory = 'G:\Logiciels et Scripts\DepotGit'
